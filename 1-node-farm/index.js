8433d85de9d83601ffc8bbeb1dcf0cb30ad2ccc1
Rhys Lloyd <rpatlloyd@gmail.com>
2023-09-10 23:54:00 +0100

    Prettier

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const slugify = require("slugify");
+
 const replaceTemplate = require("./starter/modules/replaceTemplate.js");
 
 //////////////////////////////////////////////////
@@ -55,6 +57,9 @@ const data = fs.readFileSync(
   "utf-8"
 );
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
