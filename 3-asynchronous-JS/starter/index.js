55600a409a5313a3e7fec5def10f8efeb93530c0
Rhys Lloyd <rpatlloyd@gmail.com>
2023-09-17 23:58:01 +0100

    Async await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -19,21 +19,70 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    return superagent.get(
+    const res1Pro = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-  })
-  .then((res) => {
-    console.log(res.body.message);
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then(() => {
+    const res2Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    console.log(all);
+
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file');
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+  return '2: READY';
+};
+
+(async () => {
+  try {
+    console.log('1: Will get dog pics');
+    const dogPic = await getDogPic();
+    console.log(dogPic);
+    console.log('3: Done getting dog pics');
+  } catch (error) {
+    throw error;
+  }
+})();
+
+/*
+console.log('1: Will get dog pics');
+getDogPic().then((x) => {
+  console.log(x);
+  console.log('3: Done getting dog pics');
+});
+*/
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+
+//     return superagent.get(
+//       `https://dog.ceo/api/breed/${data}/images/random`
+//     );
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then(() => {
+//     console.log('Random dog image saved to file');
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
