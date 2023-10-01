e79d29906f0806eedf0e14d37c929ca05ee369b4
Rhys Lloyd <rpatlloyd@gmail.com>
2023-10-01 22:45:44 +0100

    Mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection Established'));
 
 const app = require('./app');
 
