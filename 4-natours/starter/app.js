5eaef6aa5f4801c6a6b430d109931f69c95a2cc1
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-11-11 13:12:31 +0000

    Dealing with unhandled routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware');
@@ -28,6 +28,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl}`,
+  });
+});
 
 // 4. Export for server
 
