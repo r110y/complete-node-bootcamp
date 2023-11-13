2a355a452b388d5716e35b49c3108a3bbe3da01c
Rhys Lloyd <rpatlloyd@gmail.com>
2023-11-13 17:40:50 +0000

    Error handler refactoring

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,31 +27,14 @@ app.use((req, res, next) => {
 });
 
 // 3. Routes
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl}`,
-  // });
-
-  const err = new Error(`Can't find ${req.originalUrl}`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 // 4. Export for server
 
