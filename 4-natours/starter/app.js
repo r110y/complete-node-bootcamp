1e49ce4ea257e8edbc502b697e0c0b39cdbd410a
Rhys Lloyd <rpatlloyd@gmail.com>
2023-12-13 23:04:46 +0000

    Data sanitisation

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -11,10 +14,15 @@ const app = express();
 
 // 1. Global Middlewares
 
+// Set security HTTP Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit API requests
 const limiter = rateLimit({
   max: 100,
   windowMiliseconds: 60 * 60 * 1000,
@@ -22,8 +30,19 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Sanatise data again NoSQL query injection --- ABSOLUTELY ESSENTIAL
+app.use(mongoSanitize());
+
+// Sanatise data again XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+// Test middlewares
 
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware');
