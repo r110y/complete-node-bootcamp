0f09f95b6340941b71620f3f2bb53e179dfce041
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-12-15 14:53:37 +0000

    Embedding and child referencing guides

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -117,6 +118,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [{ type: mongoose.Schema.ObjectId, ref: User }],
   },
   {
     toJSON: { virtuals: true },
@@ -133,6 +135,16 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// BELOW: How to embed -- not practical for this application.
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(
+//     async (id) => await User.findById(id),
+//   );
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+// --- END OF HOW-TO
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document...');
