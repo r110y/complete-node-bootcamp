f9551b90db4098319159bcf1afc493c2908e0b10
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-12-21 22:58:23 +0000

    Tour indexes (2)

--- 4-natours/starter/controllers/handlerFactory.js
+++ 4-natours/starter/controllers/handlerFactory.js
@@ -108,7 +108,8 @@ exports.getAll = (Model) =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    const doc = await features.query;
+    // const doc = await features.query.explain();
 
     // SEND RESPONSE
     res.status(200).json({
