2fa515d614ec131cf79dbbedfd7fb07b34cb8048
Rhys Lloyd <rpatlloyd@gmail.com>
2024-01-01 23:28:21 +0000

    Building Tour Page Template

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,11 +1,36 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) get all tour data from collection
+  const tours = await Tour.find();
+
+  // 2) build template
+
+  // 3) render template using tour data from 1
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({
+    slug: req.params.slug,
+  }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  if (!tour) {
+    res.status(200).render('error', { title: 'Not found' });
+    return next(
+      new AppError(404, 'No tour found by that name'),
+    );
+  }
+
+  res.status(200).render('tour', {
+    tour,
+  });
+});
--- 4-natours/starter/routes/viewRoutes.js
+++ 4-natours/starter/routes/viewRoutes.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 
 router.get('/', viewsController.getOverview);
-
-router.get('/tour', viewsController.getTour);
+router.get('/tour/:slug', viewsController.getTour);
 
 module.exports = router;
