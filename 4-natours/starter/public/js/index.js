636b4eeacd3f66f2dcf42f7c89602a07529baf27
Rhys Lloyd <rpatlloyd@gmail.com>
2024-01-18 15:42:49 +0000

    Updating user data and passwords

--- 4-natours/starter/public/js/index.js
+++ 4-natours/starter/public/js/index.js
@@ -2,12 +2,19 @@
 import '@babel/polyfill';
 import { login, logout } from './login';
 import { renderTourMap } from './leaflet';
+import { updateSettings } from './updateSettings';
 
 // DOM elements
 const loginForm = document.querySelector('.form--login');
 const map = document.querySelector('#map');
 const logOutBtn = document.querySelector(
   '.nav__el--logout',
+);
+const userDataForm = document.querySelector(
+  '.form-user-data',
+);
+const userPasswordForm = document.querySelector(
+  '.form-user-password',
 );
 
 // Values
@@ -29,3 +36,30 @@ if (map) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const passwordCurrent = document.getElementById(
+      'password-current',
+    ).value;
+    const password =
+      document.getElementById('password').value;
+    const passwordConfirm = document.getElementById(
+      'password-confirm',
+    ).value;
+    updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password',
+    );
+  });
+}
