3b31e91673cac099b432e580ee60a8fb201a4327
Rhys Lloyd <rpatlloyd@gmail.com>
2024-01-23 18:01:50 +0000

    gitignore

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -54,7 +54,6 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   const url = `${req.protocol}://${req.get('host')}/me`;
-  console.log(url);
 
   await new Email(newUser, url).sendWelcome();
 
