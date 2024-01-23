ee7bfcc03cbb8887b77a04f0f912bc85ddbd9135
Rhys Lloyd <rpatlloyd@gmail.com>
2024-01-23 22:23:07 +0000

    Adding SMTP

--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const Transport = require('nodemailer-brevo-transport');
 const pug = require('pug');
 const htmlToText = require('html-to-text');
 
@@ -13,15 +14,24 @@ module.exports = class Email {
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        // service: 'Brevo',
+        host: process.env.BREVO_HOST,
+        port: process.env.BREVO_PORT,
+        auth: {
+          user: process.env.BREVO_LOGIN,
+          pass: process.env.BREVO_SMTPKEY,
+        },
+      });
     }
 
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      // service: 'Brevo',
+      host: process.env.BREVO_HOST,
+      port: process.env.BREVO_PORT,
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.BREVO_LOGIN,
+        pass: process.env.BREVO_SMTPKEY,
       },
     });
   }
